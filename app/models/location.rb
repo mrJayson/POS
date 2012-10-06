98eef94e18d9ce3616541eaa3cceee5c54dab7bf
Jason Huang <mrjaayson@gmail.com>
2012-10-06 22:42:21 +1000

    added validations to locations

--- app/models/location.rb
+++ app/models/location.rb
@@ -8,30 +8,33 @@ class Location < ActiveRecord::Base
   has_many :employees
   has_many :stocks
   has_many :transactions
+  
+  #promotions
 
   attr_accessible :name, :location_type, :max_capacity, :location_id, :stock_id
   
   validates_presence_of :name, :location_type, :max_capacity
-  validates_presence_of :location_id, :unless => :is_warehouse?
+  validates_inclusion_of :location_type, :in => ["shelf", "store", "warehouse"]
+  validates_numericality_of :max_capacity
   
+  #all locations except warehouse must have a parent location
+  validates_presence_of :location_id, :unless => :is_warehouse?
   def is_warehouse?
     :location_type == "warehouse"
   end
   
-  validates_inclusion_of :location_type, :in => ["shelf", "store", "warehouse"]
-  
-  validates_numericality_of :max_capacity
-  
+  #current_quantity cannot be greater than max_quantity
   validate :max_greater_than_current
-  
   def max_greater_than_current
-    
     if (sum_location_quantity(self) + get_marginal_quantity) > max_capacity
-      errors.add(:max_capacity, "moving too much into location")
+      errors.add(:max_capacity, "moving too much stock into location")
     end
   end
   
-  #validates_uniqueness_of :name # only if store, must be unique
-  
+  #all store names must be unique, not shelves as they are typically aisle numbers
+  validates_uniqueness_of :name, :unless => :is_shelf?
+  def is_shelf?
+    :location_type == "shelf"
+  end
   
 end
